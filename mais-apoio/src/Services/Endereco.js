dd20f6b9555c26ecbfba2dc93e020a5c2e530552
Joao Paulo <joao.paulo13186@gmail.com>
2024-11-07 22:14:10 -0300

    commit correções

--- mais-apoio/src/Services/Endereco.js
+++ mais-apoio/src/Services/Endereco.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import axios from 'axios';
 
 export default async function AcharCep(cepDigitado){
   console.log('chegou')
   return axios.get(`https://viacep.com.br/ws/${cepDigitado}/json/`)
-=======
-import axios from 'axios';
-
-export default async function AcharCep(cepDigitado){
-  return axios.get(`https://viacep.com.br/ws/${cepDigitado}/json/`)
->>>>>>> 446259f3b0b8deb5f2d75cc0ec93e0e98929605e
 }

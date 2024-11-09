b37839a77a65bbddd743b53f786195a8469427fb
Joao Paulo <joao.paulo13186@gmail.com>
2024-11-08 21:27:19 -0300

    commit de correcao na branch

--- MaisApoio/MaisApoio.Repositorio/Contexto/MaisApoioContexto.cs
+++ MaisApoio/MaisApoio.Repositorio/Contexto/MaisApoioContexto.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Data.Common;
 using System.Data.SqlClient;
 using Neo4j.Driver;
@@ -20,28 +19,4 @@ public class MaisApoioContexto
     {
         return _conexaoNeo4j.AsyncSession(o => o.WithDatabase("neo4j"));
     }
-=======
-using System.Data.Common;
-using System.Data.SqlClient;
-using Neo4j.Driver;
-
-namespace MaisApoio.MaisApoio.Repositorio.Contexto;
-
-public class MaisApoioContexto
-{
-    private readonly string _conexaoSql = "Server=.\\SQLEXPRESS;Database=MaisApoio;Trusted_Connection=True;TrustServerCertificate=True;";
-
-    private readonly IDriver _conexaoNeo4j = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "senha_do_neo4j"));
-    //Alterar                                                            localhost e porta                       Altere a senha
-
-    public DbConnection ConectarSqlServer()
-    {
-        return new SqlConnection(_conexaoSql);
-    }
-
-    public IAsyncSession ConectarNeo4j()
-    {
-        return _conexaoNeo4j.AsyncSession(o => o.WithDatabase("neo4j"));
-    }
->>>>>>> 446259f3b0b8deb5f2d75cc0ec93e0e98929605e
 }
